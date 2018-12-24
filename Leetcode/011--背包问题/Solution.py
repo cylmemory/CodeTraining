6700e121c9900067a6a28cb5ce27ed93cf104ce3
cylmemory <524545756@qq.com>
2018-12-24 23:48:32 +0800

    每日一题

--- Leetcode/011--背包问题/Solution.py
+++ Leetcode/011--背包问题/Solution.py
@@ -3,12 +3,12 @@ def canPartition(nums):
     if half_sum % 2 == 1:
         return False
     half_sum = half_sum / 2
-    d = [[False for x in xrange(half_sum + 1)] for y in xrange(len(nums) + 1)]
-    for k in xrange(len(nums) + 1):
+    d = [[False for x in range(half_sum + 1)] for y in range(len(nums) + 1)]
+    for k in range(len(nums) + 1):
         d[k][0] = True
 
-    for i in xrange(1, len(nums) + 1):
-        for j in xrange(0, half_sum + 1):
+    for i in range(1, len(nums) + 1):
+        for j in range(0, half_sum + 1):
             d[i][j] = d[i - 1][j]
             if j >= nums[i - 1]:
                 d[i][j] = d[i][j] | d[i - 1][j - nums[i - 1]]
