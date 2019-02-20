4e6d2be0a66029d6faeed8ee23fd9f45dfa2ebf9
cylmemory <524545756@qq.com>
2019-02-20 18:17:17 +0800

    update

--- 剑指offer/5.替换空格/Solution.py
+++ 剑指offer/5.替换空格/Solution.py
@@ -1,32 +1,44 @@
-class Solution:
-    def replaceSpace(self, example):
+# 题目描述：
+# 将一个字符串中的空格替换成 "%20"
+# 要求是时间复杂度 O(N)，空间复杂度 O(1)
+# example:
+# Input:
+# "A B"
+#
+# Output:
+# "A%20B"
+# 思路:
+# 1.先遍历字符串有空格符，如果要更换成"%20"，需要在尾部填充两个任意字符。
+# 2.令P1 指向字符串原来的末尾位置，P2 指向字符串现在的末尾位置，
+# P1 和 P2 从后向前遍历，当 P1 遍历到一个空格时，就需要令 P2 指向的位置依次填充 02%，否则就填充上 P1 指向字符的值
 
-        if example == None or len(example) == 0 or not isinstance(example, str):
+class Solution:
+    def replaceBlank(self, init_str):
+
+        if not isinstance(init_str, str) or len(init_str) <= 0 or init_str == None:
             return ""
 
-        str_len = len(example)
-        space_nums = 0
-        for i in example:
+        blank_nums = 0
+        for i in init_str:
             if i == " ":
-                space_nums += 1
-
-        new_str_len = str_len + space_nums*2
+                blank_nums += 1
+        init_str_len = len(init_str)
+        new_str_len = init_str_len + blank_nums*2
         new_str = new_str_len * [None]
+        init_str_index, new_str_index = init_str_len-1, new_str_len-1
 
-        indexInitial, indexNew = str_len-1, new_str_len-1
-
-        while indexInitial >= 0 and indexNew >= indexInitial:
-            if example[indexInitial] == '':
-                new_str[indexNew-2:indexNew+1] = ['%', '2', '0']
-                indexInitial -= 1
-                indexNew -= 3
+        while init_str_index >= 0 and new_str_index >= init_str_index:
+            if init_str[init_str_index] == " ":
+                new_str[new_str_index-2:new_str_index+1] = ['%', '2', '0']
+                new_str_index -= 3
+                init_str_index -= 1
             else:
-                new_str[indexNew] = example[indexInitial]
-                indexNew -= 1
-                indexInitial -= 1
+                new_str[new_str_index] = init_str[init_str_index]
+                new_str_index -= 1
+                init_str_index -= 1
         return "".join(new_str)
 
 
-s = 'we are happy'
+s = ''
 test = Solution()
-print(test.replaceSpace(s))
+print(test.replaceBlank(s))
