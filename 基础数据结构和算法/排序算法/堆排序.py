efbcd0e51e1d53618a5aaead01db274cf730d384
cylmemory <524545756@qq.com>
2019-03-15 22:18:39 +0800

    堆排序

--- 基础数据结构和算法/排序算法/堆排序.py
+++ 基础数据结构和算法/排序算法/堆排序.py
@@ -1,33 +1,26 @@
-def adjust_heap(slist, i, len_list):
+def heapSort(alist):
+    if alist == None or len(alist) == 0:
+        return
+    length = len(alist)
+    output = []
+    for i in range(length):
+        tempLen = len(alist)
+        for j in range(tempLen//2-1, -1, -1):
+            preIndex = j
+            preVal, heap = alist[preIndex], False
+            while 2 * preIndex < tempLen - 1 and not heap:
+                curIndex = 2 * preIndex + 1
+                if curIndex < tempLen - 1:
+                    if alist[curIndex] < alist[curIndex+1]:
+                        curIndex += 1
+                if preVal >= alist[curIndex]:
+                    heap = True
+                else:
+                    alist[preIndex] = alist[curIndex]
+                    preIndex = curIndex
+            alist[preIndex] = preVal
+        output.insert(0, alist.pop(0))
+    return output
 
-    k = 2 * i + 1
-    while k < len_list:
-        if k+1 < len_list and slist[k] < slist[k+1]:
-            k += 1
-        if slist[i] < slist[k]:
-            slist[i], slist[k] = slist[k], slist[i]
-            k = 2 * k + 1
-        else:
-            break
-
-
-def heap_sort(slist):
-   if slist == [] or slist is None:
-       return
-
-   i = len(slist)//2-1
-   while i >= 0:
-       adjust_heap(slist, i, len(slist))    # 构建大堆
-       i -= 1
-
-   k = len(slist)-1
-   while k > 0:
-       slist[0], slist[len(slist)-1] = slist[len(slist)-1], slist[0]
-       adjust_heap(slist, 0, len(slist)-1)   # 堆顶和末尾元素交换 and 调整堆结构
-       k -= 1
-   return slist
-
-
-test=[49, 38, 65, 97, 76, 13, 27, 49]
-
-print(heap_sort(test))
+test = [2, 6, 5, 9, 10, 3, 7]
+print(heapSort(test))
